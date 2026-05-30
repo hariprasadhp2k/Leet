77223e46a9d4d6ef2dae2df8f0657325c56ce4c2
hariprasadhp2k <hariprasadhp2k06@gmail.com>
2026-05-30 15:15:55 +0530

    Time: 4 ms (100.00%) | Memory: 46 MB (52.66%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+
+        int org = x;
+        int rev = 0;
+
+        while (x > 0) {
+            int rem = x % 10;
+            rev = rev * 10 + rem;
+            x /= 10;
+        }
+
+        return rev == org;
+    }
+}
